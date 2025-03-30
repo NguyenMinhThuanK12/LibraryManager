791d52abe3c4f935e1803292eb5fc3951f5c65f1
NgoHiu <truonghieu.29022004@gmail.com>
2025-03-30 18:55:30 +0700

    commit new

--- ProjectLibraryManager/ProjectLibraryManager/LichSuHoatDong.Designer.cs
+++ ProjectLibraryManager/ProjectLibraryManager/LichSuHoatDong.Designer.cs
@@ -29,9 +29,25 @@
         private void InitializeComponent()
         {
             panel1 = new Panel();
-            richTextBox1 = new RichTextBox();
             label1 = new Label();
+            richTextBox1 = new RichTextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            comboBox1 = new ComboBox();
+            dateTimePicker1 = new DateTimePicker();
+            panel2 = new Panel();
+            label2 = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            panel3 = new Panel();
             panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
@@ -43,17 +59,6 @@
             panel1.Size = new Size(1282, 60);
             panel1.TabIndex = 1;
             // 
-            // richTextBox1
-            // 
-            richTextBox1.BorderStyle = BorderStyle.FixedSingle;
-            richTextBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            richTextBox1.ForeColor = SystemColors.WindowFrame;
-            richTextBox1.Location = new Point(67, 131);
-            richTextBox1.Name = "richTextBox1";
-            richTextBox1.Size = new Size(400, 41);
-            richTextBox1.TabIndex = 2;
-            richTextBox1.Text = "Tìm kiếm...";
-            // 
             // label1
             // 
             label1.AutoSize = true;
@@ -64,17 +69,211 @@
             label1.TabIndex = 0;
             label1.Text = "Lịch Sử Hoạt Động";
             // 
+            // richTextBox1
+            // 
+            richTextBox1.BackColor = Color.LightGray;
+            richTextBox1.BorderStyle = BorderStyle.FixedSingle;
+            richTextBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            richTextBox1.ForeColor = SystemColors.WindowFrame;
+            richTextBox1.Location = new Point(38, 90);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(500, 41);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "Tìm kiếm...";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold);
+            button1.Location = new Point(695, 90);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 100);
+            button1.TabIndex = 3;
+            button1.Text = "Mượn Trả";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold);
+            button2.Location = new Point(885, 90);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 100);
+            button2.TabIndex = 3;
+            button2.Text = "Đặt chỗ";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold);
+            button3.Location = new Point(1065, 90);
+            button3.Name = "button3";
+            button3.Size = new Size(100, 100);
+            button3.TabIndex = 3;
+            button3.Text = "Vi phạm";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(38, 159);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(238, 31);
+            comboBox1.TabIndex = 4;
+            comboBox1.Text = "Lọc theo";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CalendarFont = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dateTimePicker1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dateTimePicker1.Format = DateTimePickerFormat.Short;
+            dateTimePicker1.Location = new Point(330, 160);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(208, 30);
+            dateTimePicker1.TabIndex = 5;
+            dateTimePicker1.ValueChanged += dateTimePicker1_ValueChanged;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.DimGray;
+            panel2.Controls.Add(label2);
+            panel2.Location = new Point(117, 262);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1052, 60);
+            panel2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(384, 10);
+            label2.Name = "label2";
+            label2.Size = new Size(281, 41);
+            label2.TabIndex = 0;
+            label2.Text = "Lịch Sử Hoạt Động";
+            label2.Click += label2_Click;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+            tableLayoutPanel1.ColumnCount = 5;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 222F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 223F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 197F));
+            tableLayoutPanel1.Controls.Add(label3, 1, 0);
+            tableLayoutPanel1.Controls.Add(label4, 0, 0);
+            tableLayoutPanel1.Controls.Add(label5, 2, 0);
+            tableLayoutPanel1.Controls.Add(label6, 3, 0);
+            tableLayoutPanel1.Controls.Add(label7, 4, 0);
+            tableLayoutPanel1.Location = new Point(117, 316);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 38.46154F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 61.53846F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 66F));
+            tableLayoutPanel1.Size = new Size(1052, 187);
+            tableLayoutPanel1.TabIndex = 6;
+            tableLayoutPanel1.Paint += tableLayoutPanel1_Paint;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Dock = DockStyle.Fill;
+            label3.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold);
+            label3.Location = new Point(207, 1);
+            label3.Name = "label3";
+            label3.Size = new Size(196, 45);
+            label3.TabIndex = 0;
+            label3.Text = "Hạn trả";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Dock = DockStyle.Fill;
+            label4.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold);
+            label4.Location = new Point(4, 1);
+            label4.Name = "label4";
+            label4.Size = new Size(196, 45);
+            label4.TabIndex = 0;
+            label4.Text = "Ngày mượn";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            label4.Click += label3_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Dock = DockStyle.Fill;
+            label5.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold);
+            label5.Location = new Point(410, 1);
+            label5.Name = "label5";
+            label5.Size = new Size(216, 45);
+            label5.TabIndex = 0;
+            label5.Text = "Ngày trả thực tế";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            label5.Click += label3_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Dock = DockStyle.Fill;
+            label6.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold);
+            label6.Location = new Point(633, 1);
+            label6.Name = "label6";
+            label6.Size = new Size(217, 45);
+            label6.TabIndex = 0;
+            label6.Text = "Trạng thái";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            label6.Click += label3_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Dock = DockStyle.Fill;
+            label7.Font = new Font("Segoe UI Semibold", 10.2F, FontStyle.Bold);
+            label7.Location = new Point(857, 1);
+            label7.Name = "label7";
+            label7.Size = new Size(191, 45);
+            label7.TabIndex = 0;
+            label7.Text = "Thông tin chi tiết";
+            label7.TextAlign = ContentAlignment.MiddleCenter;
+            label7.Click += label3_Click;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.White;
+            panel3.Location = new Point(38, 225);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(1208, 437);
+            panel3.TabIndex = 7;
+            // 
             // LichSuHoatDong
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1282, 703);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(panel2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(comboBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(richTextBox1);
             Controls.Add(panel1);
+            Controls.Add(panel3);
             Name = "LichSuHoatDong";
             Text = "LichSuHoatDong";
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -83,5 +282,19 @@
         private Panel panel1;
         private RichTextBox richTextBox1;
         private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private ComboBox comboBox1;
+        private DateTimePicker dateTimePicker1;
+        private Panel panel2;
+        private Label label2;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Panel panel3;
     }
 }
---
+++ ProjectLibraryManager/ProjectLibraryManager/QLMuonTraGUI.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace muon
+{
+    partial class QLMuonTraGUI
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label2 = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label10 = new Label();
+            label9 = new Label();
+            label8 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label7 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold);
+            label1.ForeColor = SystemColors.ControlText;
+            label1.Location = new Point(36, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(317, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Quản Lý Mượn Trả Thiết Bị";
+            label1.Click += label1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 13.5F);
+            textBox1.ForeColor = SystemColors.ScrollBar;
+            textBox1.Location = new Point(631, 26);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(426, 34);
+            textBox1.TabIndex = 1;
+            textBox1.Text = "  Tìm kiếm...";
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.MediumSeaGreen;
+            button1.Font = new Font("Segoe UI", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonFace;
+            button1.Location = new Point(1091, 23);
+            button1.Name = "button1";
+            button1.Size = new Size(171, 38);
+            button1.TabIndex = 2;
+            button1.Text = "Thêm phiếu mượn";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            radioButton1.Location = new Point(294, 36);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(208, 27);
+            radioButton1.TabIndex = 3;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Xem theo phiếu mượn";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            radioButton2.Location = new Point(761, 36);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(194, 27);
+            radioButton2.TabIndex = 4;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Xem theo thành viên";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(panel2);
+            panel1.Controls.Add(tableLayoutPanel1);
+            panel1.Controls.Add(radioButton2);
+            panel1.Controls.Add(radioButton1);
+            panel1.Location = new Point(-1, 83);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1286, 631);
+            panel1.TabIndex = 6;
+            panel1.Paint += panel1_Paint;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.DarkGray;
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(label2);
+            panel2.Location = new Point(0, 125);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1286, 44);
+            panel2.TabIndex = 6;
+            panel2.Paint += panel2_Paint;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(484, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(313, 37);
+            label2.TabIndex = 0;
+            label2.Text = "Danh Sách Phiếu Mượn";
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
+            tableLayoutPanel1.ColumnCount = 8;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 14F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 14F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 14F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 14F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 14F));
+            tableLayoutPanel1.Controls.Add(label10, 7, 0);
+            tableLayoutPanel1.Controls.Add(label9, 6, 0);
+            tableLayoutPanel1.Controls.Add(label8, 5, 0);
+            tableLayoutPanel1.Controls.Add(label6, 3, 0);
+            tableLayoutPanel1.Controls.Add(label5, 2, 0);
+            tableLayoutPanel1.Controls.Add(label4, 1, 0);
+            tableLayoutPanel1.Controls.Add(label3, 0, 0);
+            tableLayoutPanel1.Controls.Add(label7, 4, 0);
+            tableLayoutPanel1.Location = new Point(0, 166);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 1;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new Size(1286, 48);
+            tableLayoutPanel1.TabIndex = 5;
+            // 
+            // label10
+            // 
+            label10.Dock = DockStyle.Fill;
+            label10.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label10.Location = new Point(1104, 1);
+            label10.Name = "label10";
+            label10.Size = new Size(178, 46);
+            label10.TabIndex = 7;
+            label10.Text = "Xem thêm";
+            label10.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            label9.Dock = DockStyle.Fill;
+            label9.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label9.Location = new Point(925, 1);
+            label9.Name = "label9";
+            label9.Size = new Size(172, 46);
+            label9.TabIndex = 6;
+            label9.Text = "Trạng thái mượn";
+            label9.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            label8.Dock = DockStyle.Fill;
+            label8.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label8.Location = new Point(746, 1);
+            label8.Name = "label8";
+            label8.Size = new Size(172, 46);
+            label8.TabIndex = 5;
+            label8.Text = "Tổng tiền";
+            label8.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Dock = DockStyle.Fill;
+            label6.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label6.Location = new Point(439, 1);
+            label6.Name = "label6";
+            label6.Size = new Size(121, 46);
+            label6.TabIndex = 3;
+            label6.Text = "Hạn trả";
+            label6.TextAlign = ContentAlignment.MiddleCenter;
+            label6.Click += label6_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Dock = DockStyle.Fill;
+            label5.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label5.Location = new Point(311, 1);
+            label5.Name = "label5";
+            label5.Size = new Size(121, 46);
+            label5.TabIndex = 2;
+            label5.Text = "Ngày mượn";
+            label5.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Dock = DockStyle.Fill;
+            label4.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label4.Location = new Point(132, 1);
+            label4.Name = "label4";
+            label4.Size = new Size(172, 46);
+            label4.TabIndex = 1;
+            label4.Text = "Thành viên";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            label4.Click += label4_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Dock = DockStyle.Fill;
+            label3.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label3.Location = new Point(4, 1);
+            label3.Name = "label3";
+            label3.Size = new Size(121, 46);
+            label3.TabIndex = 0;
+            label3.Text = "Mã phiếu";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            label7.Dock = DockStyle.Fill;
+            label7.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            label7.Location = new Point(567, 1);
+            label7.Name = "label7";
+            label7.Size = new Size(172, 46);
+            label7.TabIndex = 4;
+            label7.Text = "Ngày trả thực tế";
+            label7.TextAlign = ContentAlignment.MiddleCenter;
+            label7.Click += label7_Click;
+            // 
+            // QLMuonTraGUI
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1284, 711);
+            Controls.Add(panel1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "QLMuonTraGUI";
+            RightToLeft = RightToLeft.No;
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Panel panel1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Panel panel2;
+        private Label label2;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+    }
+}
