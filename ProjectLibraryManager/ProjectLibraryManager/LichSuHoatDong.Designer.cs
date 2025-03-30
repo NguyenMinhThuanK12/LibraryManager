30396751072930129eb86e322f6467e4aa5262b3
NgoHiu <truonghieu.29022004@gmail.com>
2025-03-30 18:01:53 +0700

    commit

---
+++ ProjectLibraryManager/ProjectLibraryManager/LichSuHoatDong.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace ProjectLibraryManager
+{
+    partial class LichSuHoatDong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            richTextBox1 = new RichTextBox();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.LightGray;
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1282, 60);
+            panel1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BorderStyle = BorderStyle.FixedSingle;
+            richTextBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            richTextBox1.ForeColor = SystemColors.WindowFrame;
+            richTextBox1.Location = new Point(67, 131);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(400, 41);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "Tìm kiếm...";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(281, 41);
+            label1.TabIndex = 0;
+            label1.Text = "Lịch Sử Hoạt Động";
+            // 
+            // LichSuHoatDong
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1282, 703);
+            Controls.Add(richTextBox1);
+            Controls.Add(panel1);
+            Name = "LichSuHoatDong";
+            Text = "LichSuHoatDong";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private RichTextBox richTextBox1;
+        private Label label1;
+    }
+}
